71bc131de415b2cc789c605893d8848e8d61f7a8
zavala32 <nick2kobe@gmail.com>
2018-02-20 13:22:51 -0800

    Signed-off-by: zavala32 <nick2kobe@gmail.com>

---
+++ CPPTemplate/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on July 19, 2016, 9:07 AM
+ * Purpose:  Hello World Template
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process/Calculations Here
+    
+    //Output Located Here
+    cout<<"Hello World"<<endl;
+
+    //Exit
+    return 0;
+}
+
