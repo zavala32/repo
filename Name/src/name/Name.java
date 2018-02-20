43438b65dca1d5eab7fcab702549f8b41e657628
zavala32 <nick2kobe@gmail.com>
2018-02-20 13:59:25 -0800

    my first example Signed-off-by: zavala32 <nick2kobe@gmail.com>

---
+++ Name/src/name/Name.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package name;
+
+/**
+ *#include <iostream>
+ using namespace std;
+ * @author rcc
+ */
+public class Name {
+
+    /**
+     * @param args What"s my name
+     */
+    public static void main(String[] args) {
+        System.out.println("My Name Is Nicolas Zavala");
+    }
+    
+}
