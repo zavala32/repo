1864697f9249071072fcaf0a8eec887366c24b9b
zavala32 <nick2kobe@gmail.com>
2018-03-07 11:29:17 -0800

    Add files via upload

---
+++ Letter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package letter;
+
+/**
+ *#include <iostream>
+using namespace std;
+ * @author nick2
+ */
+public class Letter {
+char letter;
+
+}
+
+    /**
+     *cout<<"please enter a character : "<<endl;@
+     *cin>>letter;
+    *cout<<staic_cast<int>(letter)<<endl;
+    \**
+    return 0;
+        // TODO code application logic here
+    }
+    
+}
